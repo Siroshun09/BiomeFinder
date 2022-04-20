892d96e1b10fb29cc90f75fd19226fa0c4033262
Siroshun09 <siro.shun8@gmail.com>
2022-04-21 08:27:58 +0900

    build: update paperweight-userdev to 1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "1.3.4"
+    id("io.papermc.paperweight.userdev") version "1.3.5"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
