56fb94051c103902dbeb2a51716562c96d4fa9f4
Siroshun09 <siro.shun8@gmail.com>
2022-08-28 12:11:06 +0900

    feat: implements SeedGenerator

--- src/main/java/com/github/siroshun09/biomefinder/util/NMSUtils.java
+++ src/main/java/com/github/siroshun09/biomefinder/util/NMSUtils.java
@@ -1,11 +1,16 @@
 package com.github.siroshun09.biomefinder.util;
 
+import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.world.level.biome.BiomeSource;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.presets.WorldPresets;
+import net.minecraft.world.level.levelgen.synth.NormalNoise;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_19_R1.CraftServer;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +18,20 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public final class NMSUtils {
+
+    public static @NotNull BiomeSource getBiomeSource() {
+        return MultiNoiseBiomeSource.Preset.OVERWORLD.biomeSource(BuiltinRegistries.BIOME);
+    }
+
+    public static @NotNull NoiseGeneratorSettings getNoiseGeneratorSettings(boolean large) {
+        return BuiltinRegistries.NOISE_GENERATOR_SETTINGS.getOrCreateHolderOrThrow(
+                large ? NoiseGeneratorSettings.LARGE_BIOMES : NoiseGeneratorSettings.OVERWORLD
+        ).value();
+    }
+
+    public static @NotNull Registry<NormalNoise.NoiseParameters> getNoiseParameters() {
+        return BuiltinRegistries.NOISE;
+    }
 
     public static @NotNull ChunkGenerator getChunkGenerator(long seed, @NotNull Dimension dimension, boolean large) {
         var preset = BuiltinRegistries.WORLD_PRESET.get(large ? WorldPresets.LARGE_BIOMES : WorldPresets.NORMAL);
---
+++ src/main/java/com/github/siroshun09/biomefinder/util/SeedGenerator.java
@@ -0,0 +1,52 @@
+package com.github.siroshun09.biomefinder.util;
+
+import net.minecraft.core.QuartPos;
+import net.minecraft.world.level.levelgen.RandomState;
+import org.bukkit.Location;
+import org.bukkit.block.Biome;
+import org.bukkit.craftbukkit.v1_19_R1.util.CraftNamespacedKey;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Random;
+
+public final class SeedGenerator {
+
+    public static long generateSeedWithFixedSpawnBiome(@NotNull Biome biome) {
+        return generateSeedWithFixedSpawnBiome(biome, new Location(null, 0.0, 0.0, 0.0), false, 10000);
+    }
+
+    public static long generateSeedWithFixedSpawnBiome(@NotNull Biome biome, @NotNull Location spawnLocation,
+                                                       boolean large, int maxAttempts) {
+        var random = new Random();
+
+        int biomeX = QuartPos.fromBlock(spawnLocation.getBlockX());
+        int biomeY = QuartPos.fromBlock(spawnLocation.getBlockY());
+        int biomeZ = QuartPos.fromBlock(spawnLocation.getBlockZ());
+
+        var biomeResourceLocation = CraftNamespacedKey.toMinecraft(biome.getKey());
+        var biomeSource = NMSUtils.getBiomeSource();
+        var settings = NMSUtils.getNoiseGeneratorSettings(large);
+
+        long seed;
+        int attempts = 0;
+
+        while (attempts < maxAttempts) {
+            seed = random.nextLong();
+
+            var state = RandomState.create(settings, NMSUtils.getNoiseParameters(), seed);
+            var spawnBiome = biomeSource.getNoiseBiome(biomeX, biomeY, biomeZ, state.sampler());
+
+            if (spawnBiome.is(biomeResourceLocation)) {
+                return seed;
+            } else {
+                attempts++;
+            }
+        }
+
+        return -1;
+    }
+
+    private SeedGenerator() {
+        throw new UnsupportedOperationException();
+    }
+}
