9ca906de955009b49bbcd05dd9158856f1876116
Siroshun09 <siro.shun8@gmail.com>
2025-06-01 14:00:33 +0900

    feat: add a commonized method for generating seed with conditions

--- src/main/java/com/github/siroshun09/biomefinder/util/SeedGenerator.java
+++ src/main/java/com/github/siroshun09/biomefinder/util/SeedGenerator.java
@@ -1,35 +1,44 @@
 package com.github.siroshun09.biomefinder.util;
 
+import com.github.siroshun09.biomefinder.wrapper.biome.BiomeSource;
 import com.github.siroshun09.biomefinder.wrapper.biome.MultiNoiseBiomeSourceWrapper;
 import net.kyori.adventure.key.Key;
 import org.jspecify.annotations.NullMarked;
 
 import java.util.Random;
+import java.util.function.Predicate;
 
 @NullMarked
 public final class SeedGenerator {
 
     public static long generateSeedWithFixedSpawnBiome(Key key) {
-        return generateSeedWithFixedSpawnBiome(key, 0, 0, 0, false, 10000);
+        return generateSeedByCondition(source -> {
+            Key biomeKey = source.getBiome(0, 0, 0);
+            return key.equals(biomeKey);
+        }, false, 10000);
     }
 
     public static long generateSeedWithFixedSpawnBiome(Key key, int x, int y, int z,
                                                        boolean large, int maxAttempts) {
-        var random = new Random();
+        return generateSeedByCondition(source -> {
+            Key biomeKey = source.getBiome(x, y, z);
+            return key.equals(biomeKey);
+        }, large, maxAttempts);
+    }
+
+    public static long generateSeedByCondition(Predicate<BiomeSource> predicate, boolean large, int maxAttempts) {
+        Random random = new Random();
 
         int attempts = 0;
 
         while (attempts < maxAttempts) {
             long seed = random.nextLong();
 
-            var biomeSource = large ? MultiNoiseBiomeSourceWrapper.largeBiomes(seed) : MultiNoiseBiomeSourceWrapper.overworld(seed);
-            var spawnBiome = biomeSource.getBiome(x, y, z);
-
-            if (key.equals(spawnBiome)) {
+            BiomeSource biomeSource = large ? MultiNoiseBiomeSourceWrapper.largeBiomes(seed) : MultiNoiseBiomeSourceWrapper.overworld(seed);
+            if (predicate.test(biomeSource)) {
                 return seed;
-            } else {
-                attempts++;
             }
+            attempts++;
         }
 
         return -1;
